b1531a67e3531875d2141f4c3c8c92b5d3b046fa
Jaman Dedy <jeandedieuam@gmail.com>
2019-12-04 11:34:34 +0200

    Programmatic navigation

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -34,9 +34,12 @@ class Blog extends Component {
           </nav>
         </header>
         {/* <Route path="/" exact render={() => <h1>Home</h1>} /> */}
-        <Route path="/" exact component={Posts} />
-        <Route path="/new-post" component={NewPost} />
-        <Route path="/:id" exact component={FullPost} />
+        <Switch>
+          {" "}
+          <Route path="/" exact component={Posts} />
+          <Route path="/new-post" component={NewPost} />
+          <Route path="/:id" exact component={FullPost} />
+        </Switch>
       </div>
     );
   }
