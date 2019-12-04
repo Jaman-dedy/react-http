8c848690bc4e2332a91da4b1041a6e51bc4d4935
Jaman Dedy <jeandedieuam@gmail.com>
2019-12-04 11:26:39 +0200

    Route Switch component

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Posts from "./Posts/Posts";
-import { Route, NavLink } from "react-router-dom";
+import { Route, NavLink, Switch } from "react-router-dom";
 
 import "./Blog.css";
 import NewPost from "./NewPost/NewPost";
